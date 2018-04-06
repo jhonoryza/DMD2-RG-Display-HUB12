af98685a6d3690441c65958669ad5087cbafdffd
fajar <fajar@gti.co.id>
2018-04-06 15:44:41 +0700

    edit readme and example

--- examples/ScrollingAlphabet/ScrollingAlphabet.ino
+++ examples/ScrollingAlphabet/ScrollingAlphabet.ino
@@ -17,15 +17,22 @@ const uint8_t *FONT = Arial14;
 
 const char *MESSAGE = "abcdefghijklmnopqrstuvwxyz";
 
-SoftDMD dmd(WIDTH,1);  // DMD controls the entire display
+SoftDMD dmd(WIDTH, 1, 2, 16, 12, 0, 14, 13, 4);  // DMD controls the entire display
 DMD_TextBox box(dmd);  // "box" provides a text box to automatically write to/scroll the display
 
 // the setup routine runs once when you press reset:
 void setup() {
   Serial.begin(9600);
-  dmd.setBrightness(255);
+  dmd.setColor("kuning");
   dmd.selectFont(FONT);
   dmd.begin();
+  const uint8_t period = 5000;
+  uint32 pwm_duty_init[1] = {135};
+  uint32 io_info[1][3] = {
+    {PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5, 5}
+  };
+  pwm_init(period, pwm_duty_init, 1, io_info);
+  pwm_start();
 }
 
 // the loop routine runs over and over again forever:
